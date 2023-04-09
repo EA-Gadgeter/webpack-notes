105e352ae0ff2baa0bf6f71147446861af72dce7
EA-Gadgeter <emiacevedoposos@gmail.com>
2023-04-08 18:21:58 -0600

    Added image copy, fonts copy. Added contentHash for files. Added minimize configurations. Added alias imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 /** @type {import('webpack').Configuration} */
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
+        path: path.resolve(__dirname, "./dist"),
+        filename: "./[name][contenthash].js"
     },
     resolve: {
-        extensions: [".js"]
+        extensions: [".js"],
+        alias: {
+            "@utils": path.resolve(__dirname, "./src/utils/"),
+            "@templates": path.resolve(__dirname, "./src/templates/"),
+            "@styles": path.resolve(__dirname, "./src/styles/"),
+            "@images": path.resolve(__dirname, "./src/assets/images/")
+        }
     },
     module: {
         rules: [
@@ -29,15 +36,38 @@ module.exports = {
                     "css-loader",
                     "stylus-loader"
                 ]
+            },
+            {
+                test: /\.png/,
+                type: "asset/resource",
+                generator: {
+                    filename: "./assets/images/[name][contenthash][ext]"
+                }
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                type: "asset/resource",
+                generator: {
+                  filename: "./assets/fonts/[name][contenthash][ext]"
+                }
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            inject: true,
+            inject: "body",
             template: "./public/index.html",
             filename: "./index.html"
         }),
-        new MiniCssExtractPlugin(),
-    ]
+        new MiniCssExtractPlugin({
+            filename: "./styles/[name][contenthash].css"
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin()
+        ]
+    }
 };
